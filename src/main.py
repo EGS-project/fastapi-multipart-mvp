925590efa6ac476c6971851d29fc6ae0b43dd133
Sevelantis <hamer223@o2.pl>
2023-04-03 13:03:50 +0100

    allow CORS

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 '''main.py is a root of the project, which inits the FastAPI app'''
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from src.conversion.router import conversion_router
 
 app = FastAPI(openapi_url="/api/v1/openapi.yaml")
-    
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
     
 app.include_router(router=conversion_router)
 
