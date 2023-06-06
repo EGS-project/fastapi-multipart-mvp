20391d8f8ae1dba2bc99e98d3dde12a019b4126a
Sevelantis <hamer223@o2.pl>
2023-06-06 15:39:18 +0100

    added POST login

--- src/conversion/router.py
+++ src/conversion/router.py
@@ -10,18 +10,21 @@ from starlette.responses import JSONResponse
 from src.conversion.schemas import ConversionCreate, ConversionRead
 
 conversion_router = APIRouter()
+import time
 
 
-@conversion_router.post('/api/v1/convert/ok')
+@conversion_router.post('/api/v1/convert')
 async def convert_ok(
     conversion_create: ConversionCreate = Depends(ConversionCreate),
     image: UploadFile = File(),
-):
+    ):
     
     conv_read = ConversionRead(
         target_format=conversion_create.target_format,
         link =f'http://loalhost:123123/here_is_your_super_download_link_.{conversion_create.target_format}'
         )
+    time.sleep(3)
+
         
     return Response(
         status_code=HTTPStatus.OK,
@@ -29,19 +32,7 @@ async def convert_ok(
         )
 
 
-@conversion_router.post('/api/v1/convert/bad')
-async def convert_bad(
-    conversion_create: ConversionCreate = Depends(ConversionCreate),
-    image: UploadFile = File(),
-):
-    # any code that is not 200
-    return Response(
-        status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-        content="1000 character exception message"
-        )
-
-
-@conversion_router.get('/api/v1/login/ok')
+@conversion_router.post('/api/v1/login')
 async def login_ok():
     response = Response(status_code=HTTPStatus.OK,content="Authorized.")
     response.set_cookie(
@@ -55,12 +46,4 @@ async def login_ok():
 
     return response
 
-
-@conversion_router.get('/api/v1/login/bad')
-async def login_bad():
-
-    return Response(
-        status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-        content="1000 character exception message"
-        )
 
